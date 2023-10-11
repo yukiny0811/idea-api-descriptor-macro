b8d4a9b4268109af138c3ca55c8ce8bbbe5da6e9
yukiny0811 <yukiny0811@gmail.com>
2023-10-12 05:50:01 +0900

    完成！

--- Sources/IdeaAPIDescriptorClient/main.swift
+++ Sources/IdeaAPIDescriptorClient/main.swift
@@ -1,8 +1,21 @@
 import IdeaAPIDescriptor
 
-let a = 17
-let b = 25
+// case API名 = その説明
 
-let (result, code) = #stringify(a + b)
+@IdeaAPIDescriptor
+enum FeatureForIdea: String, Codable, CaseIterable {
+    case WatchKit = "Apple Watch用のアプリを作れるよ！"
+    case WeatherKit = "天気の情報を使えるよ！"
+    case Vision = "画像処理とかができるよ！"
+    case API名 = "説明"
+}
 
-print("The value \(result) was produced by the code \"\(code)\"")
+// 使い方
+print(FeatureForIdea.WatchKit.name()) //WatchKit
+print(FeatureForIdea.WeatherKit.description()) //天気の情報を使えるよ！
+print(FeatureForIdea.from(name: "Vision")!.name()) //Vision
+print(FeatureForIdea.from(name: "API名")!.description()) //説明
+print(FeatureForIdea.allCases.randomElement()!)
+
+
+
--- Sources/IdeaAPIDescriptorMacros/IdeaAPIDescriptorMacro.swift
+++ Sources/IdeaAPIDescriptorMacros/IdeaAPIDescriptorMacro.swift
@@ -3,31 +3,75 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
-    public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
+internal struct IdeaData {
+    var name: String
+    var description: String
+}
 
-        return "(\(argument), \(literal: argument.description))"
+public struct IdeaAPIDescriptor: MemberMacro {
+    public static func expansion(
+        of node: SwiftSyntax.AttributeSyntax,
+        providingMembersOf declaration: some SwiftSyntax.DeclGroupSyntax,
+        in context: some SwiftSyntaxMacros.MacroExpansionContext
+    ) throws -> [SwiftSyntax.DeclSyntax] {
+        
+        let cases: [IdeaData] = declaration.memberBlock.members
+            .map { memberBlockItem in
+                let name = memberBlockItem
+                    .decl
+                    .as(EnumCaseDeclSyntax.self)!
+                    .elements
+                    .first!
+                    .name
+                    .text
+                let description = memberBlockItem
+                    .decl
+                    .as(EnumCaseDeclSyntax.self)!
+                    .elements
+                    .first!
+                    .rawValue!
+                    .value
+                    .as(StringLiteralExprSyntax.self)!
+                    .segments
+                    .first!
+                    .description
+                return IdeaData(name: name, description: description)
+            }
+        
+        var nameMethod = "" + "\n"
+        nameMethod += "func name() -> String {" + "\n"
+        nameMethod += "    switch self {" + "\n"
+        for c in cases {
+            nameMethod += "    case .\(c.name):" + "\n"
+            nameMethod += "        return \"\(c.name)\"" + "\n"
+        }
+        nameMethod += "    }" + "\n"
+        nameMethod += "}" + "\n"
+        
+        var descriptionMethod = "" + "\n"
+        descriptionMethod += "func description() -> String {" + "\n"
+        descriptionMethod += "    return self.rawValue" + "\n"
+        descriptionMethod += "}" + "\n"
+        
+        var fromMethod = "" + "\n"
+        fromMethod += "static func from(name: String) -> Self? {" + "\n"
+        fromMethod += "    switch name {" + "\n"
+        for c in cases {
+            fromMethod += "    case \"\(c.name)\":" + "\n"
+            fromMethod += "        return .\(c.name)" + "\n"
+        }
+        fromMethod += "    default:" + "\n"
+        fromMethod += "        return nil" + "\n"
+        fromMethod += "    }" + "\n"
+        fromMethod += "}" + "\n"
+        
+        return [DeclSyntax(stringLiteral: nameMethod + descriptionMethod + fromMethod)]
     }
 }
 
 @main
 struct IdeaAPIDescriptorPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
+        IdeaAPIDescriptor.self,
     ]
 }
