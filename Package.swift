929f6c73e47268168215f177174001801dd02408
yukiny0811 <yukiny0811@gmail.com>
2023-10-12 04:50:36 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -39,14 +39,5 @@ let package = Package(
 
         // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "IdeaAPIDescriptorClient", dependencies: ["IdeaAPIDescriptor"]),
-
-        // A test target used to develop the macro implementation.
-        .testTarget(
-            name: "IdeaAPIDescriptorTests",
-            dependencies: [
-                "IdeaAPIDescriptorMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-            ]
-        ),
     ]
 )
